9c02f2559fd78b7fc0dfbb79eb8041927cbadf5f
Giulia Vasconcelos <giuliavasconcelos620@gmail.com>
2024-05-17 10:06:35 -0300

    ..

---
+++ Livro React Native/cap_9a_ajax/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import UsuarioGithub from './components/UsuarioGithub';
+
+export default function App() {
+  return (
+  <UsuarioGithub />
+  );
+}
+
+
+
+
